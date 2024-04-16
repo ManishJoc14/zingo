f11181c0af0ed25739e766a1967ab309a705053c
ManishJoc14 <manishjoc14@gmail.com>
2024-04-16 10:10:21 +0545

    Revert "-"
    This reverts commit 190695a4a288ed1fa5fd20ccec20f7804b290228.

--- server/index.js
+++ server/index.js
@@ -5,18 +5,13 @@ const { initializeSocket } = require("./socket");
 
 // Initialize Express app and server
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  // Initialize socket.io
-  initializeSocket(server);
-  res.send("Hello World!");
-});
+// Initialize socket.io
+initializeSocket(server);
 
 // Set port
 const PORT = process.env.PORT || 3001;
