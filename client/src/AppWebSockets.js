8aee581d173f040e8b970642325d14a531bc49a8
Manish Joshi <120154327+ManishJoc14@users.noreply.github.com>
2025-08-05 23:01:38 +0545

    Update AppWebSockets.js

--- client/src/AppWebSockets.js
+++ client/src/AppWebSockets.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
-const socket = io("http://localhost:3001");
+const socket = io("https://zingo-backend.onrender.com");
 
 function App() {
   const [userName, setUserName] = useState("");
